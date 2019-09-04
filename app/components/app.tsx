d715e0951b716a2def4c48c2ef90ca85e4e08a0d
Thomas Röggla <t.roggla@cwi.nl>
2019-09-04 19:09:23 +0200

    rendering thank you page once closeSession is invoked

--- app/components/app.tsx
+++ app/components/app.tsx
@@ -3,16 +3,40 @@ import { useState } from "react";
 
 import ExperimentLoader from "./experiment_loader";
 import Experiment, { ExperimentStep } from "./experiment";
+import FullscreenMessage from "./fullscreen_message";
 
 const App: React.FC = () => {
+  const [sessionClosed, setSessionClosed] = useState(false);
   const [experiment, setExperiment] = useState<Array<ExperimentStep>>([]);
+
+  const renderContent = () => {
+    if (sessionClosed) {
+      return (
+        <FullscreenMessage
+          message="Thank you!"
+        />
+      );
+    }
+
+    if (experiment.length === 0) {
+      return (
+        <ExperimentLoader
+          onDataLoaded={setExperiment}
+        />
+      );
+    } else {
+      return (
+        <Experiment
+          steps={experiment}
+          closeSession={setSessionClosed.bind(null, true)}
+        />
+      );
+    }
+  };
 
   return (
     <div>
-      {(experiment.length === 0)
-        ? <ExperimentLoader onDataLoaded={setExperiment} />
-        : <Experiment steps={experiment} closeSession={() => setExperiment([])} />
-      }
+      {renderContent()}
     </div>
   );
 };
