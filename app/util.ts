234763f05875cc79bf0bedb847b432be26efb057
Thomas Röggla <t.roggla@cwi.nl>
2019-09-11 15:54:52 +0200

    added utility function to shuffle an array

--- app/util.ts
+++ app/util.ts
@@ -7,3 +7,14 @@ export function Range(start: number, end: number, step = 1): Array<number> {
     return (i % step === 0) ? result.concat([i + start]) : result;
   }, []);
 }
+
+export function shuffle<T>(arr: Array<T>): Array<T> {
+  const copy = [...arr];
+
+  for (let i = copy.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [copy[i], copy[j]] = [copy[j], copy[i]];
+  }
+
+  return copy;
+}
