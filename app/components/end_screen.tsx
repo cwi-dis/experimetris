6843f54325df536dda295e1b2f098702c2eb5130
Thomas Röggla <t.roggla@cwi.nl>
2019-10-16 16:48:14 +0200

    extracting participant ID for filename if present in EndScreen

--- app/components/end_screen.tsx
+++ app/components/end_screen.tsx
@@ -9,13 +9,18 @@ const EndScreen: React.FC<EndScreenProps> = (props) => {
   const { downloadableData, message } = props;
   const data = "data:application/json;base64," + btoa(JSON.stringify(downloadableData));
 
+  const participantId = downloadableData.find((entry) => entry.participantId);
+  const filename = (participantId)
+    ? `data_${participantId.participantId}.json`
+    : "data.json";
+
   return (
     <div id="end-screen">
       <h2 className="centered">
         {message}
       </h2>
 
-      <a download="data.json" href={data} className="button is-info">
+      <a download={filename} href={data} className="button is-info">
         Download data
       </a>
     </div>
