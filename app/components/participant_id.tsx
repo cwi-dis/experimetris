f78a1b625c0b1788be3b6b2cc18d2ba4e35bc48a
Thomas Röggla <t.roggla@cwi.nl>
2019-10-16 16:05:52 +0200

    implemented component for assigning participant ID

---
+++ app/components/participant_id.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import { useState } from "react";
+
+interface ParticipantIdResult {
+  participantId: string;
+}
+
+interface ParticipantIdProps {
+  onContinue: (data: ParticipantIdResult) => void;
+}
+
+const ParticipantId: React.FC<ParticipantIdProps> = (props) => {
+  const { onContinue } = props;
+  const [participantId, setParticipantId] = useState<string>("");
+
+  return (
+    <div className="participantid">
+      <h4 className="title is-4" style={{ color: "#DDDDDD" }}>
+        Participant ID
+      </h4>
+
+      <div className="field">
+        <div className="control">
+          <input
+            type="text"
+            className="input is-info"
+            value={participantId}
+            onChange={(e) => setParticipantId(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <button
+        className="button is-info"
+        disabled={participantId.length === 0}
+        onClick={() => onContinue({ participantId })}
+      >
+        Continue
+      </button>
+    </div>
+  );
+};
+
+export default ParticipantId;
